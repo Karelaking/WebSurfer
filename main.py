e68a2e7dcf841bf247e8168364d0b459aea2e121
Katiyarkarelaking <karelaking277@gmail.com>
2024-02-27 01:13:58 +0530

    Added Some Important Files.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import validators
+import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QIcon
@@ -21,69 +22,78 @@ class Ui_MainWindow(object):
         self.browser = None
 
     def setupUi(self, MainWindow):
+        # Main Ui Initialization
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1046, 640)
+        MainWindow.resize(1200, 800)
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl(Asc.mainUrl))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralWidget")
         MainWindow.setCentralWidget(self.browser)
 
+        # Seating Up Tool Bar
         self.toolBar = QtWidgets.QToolBar(MainWindow)
         self.toolBar.setToolTip("")
         self.toolBar.setMovable(False)
         self.toolBar.setObjectName("toolBar")
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
 
+        # Tool Bar Back Action Button
         self.actionBack = QtWidgets.QAction(MainWindow)
         self.actionBack.triggered.connect(self.browser.back)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(Asc.backIcon), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionBack.setIcon(icon)
-        self.actionBack.setObjectName("actionBack")
+        self.actionBack.setObjectName(Asc.actionBack)
 
+        # Tool Bar Forward Action Button
         self.actionForward = QtWidgets.QAction(MainWindow)
         self.actionForward.triggered.connect(self.browser.forward)
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap(Asc.forwardIcon), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionForward.setIcon(icon1)
-        self.actionForward.setObjectName("actionForward")
+        self.actionForward.setObjectName(Asc.actionForward)
 
+        # Tool Bar Reload Button
         self.actionReload = QtWidgets.QAction(MainWindow)
         self.actionReload.triggered.connect(self.browser.reload)
         icon2 = QtGui.QIcon()
         icon2.addPixmap(QtGui.QPixmap(Asc.reloadIcon), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionReload.setIcon(icon2)
-        self.actionReload.setObjectName("actionReload")
+        self.actionReload.setObjectName(Asc.actionReload)
 
+        # Tool Bar Home Navigation Button
         self.actionHome = QtWidgets.QAction(MainWindow)
         self.actionHome.triggered.connect(self.navigate_home)
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap(Asc.homeIcon), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionHome.setIcon(icon3)
+        self.actionHome.setObjectName(Asc.actionHome)
 
+        # Tool Bar Stop Action Button
+        self.actionStop = QAction(MainWindow)
+        self.actionStop.triggered.connect(self.browser.stop)
+        icon4 = QtGui.QIcon()
+        icon4.addPixmap(QtGui.QPixmap(Asc.stopIcon), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionStop.setIcon(icon4)
+        self.actionStop.setObjectName(Asc.actionStop)
+
+        # Tool Bar Url Bar Line Edit
         self.actionUrlBar = QLineEdit()
         self.actionUrlBar.setMaximumHeight(70)
         self.actionUrlBar.setMaximumHeight(100)
         self.actionUrlBar.returnPressed.connect(self.navigate_to_url)
         self.browser.urlChanged.connect(self.update_url)
 
-        icon3 = QtGui.QIcon()
-        icon3.addPixmap(QtGui.QPixmap(Asc.homeIcon), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.actionHome.setIcon(icon3)
-        self.actionHome.setObjectName("actionHome")
-
-        self.actionStop = QAction(MainWindow)
-        self.actionStop.triggered.connect(self.browser.stop)
-        icon4 = QtGui.QIcon()
-        icon4.addPixmap(QtGui.QPixmap(Asc.stopIcon), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.actionStop.setIcon(icon4)
-        self.actionStop.setObjectName("actionStop")
-
+        # Tool Bar Menu Action Button
         self.actionMenu = QAction(MainWindow)
         self.actionMenu.triggered.connect(self.print_page)
         icon5 = QtGui.QIcon()
         icon5.addPixmap(QtGui.QPixmap(Asc.menuIcon), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionMenu.setIcon((icon5))
-        self.actionMenu.setObjectName("actionMenu")
+        self.actionMenu.setObjectName(Asc.actionMenu)
 
+        # Adding All Elements In Tool Bar
         self.toolBar.addAction(self.actionBack)
         self.toolBar.addSeparator()
         self.toolBar.addAction(self.actionForward)
@@ -107,7 +117,8 @@ class Ui_MainWindow(object):
         if validators.url(f"https://{url}"):
             self.browser.setUrl(QUrl(f"https://{url}"))
         else:
-            self.browser.setUrl(QUrl(f"{Asc.googleMainUrl}/search?q={url.replace(" ", "+")}"))
+            # self.browser.setUrl(QUrl(f"{Asc.googleMainUrl}/search?q={url.replace(" ", "+")}"))
+            self.browser.setUrl(QUrl(f"{Asc.mainUrl}/?va=e&t=hh&q={url.replace(" ", "+")}&ia=web"))
 
     def update_url(self, q):
         self.actionUrlBar.setText(q.toString())
@@ -119,23 +130,21 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
-        self.actionBack.setText(_translate("MainWindow", "Back"))
+        self.actionBack.setText(_translate("MainWindow", Asc.back))
         self.actionBack.setToolTip(_translate("MainWindow", "Go Back"))
         self.actionBack.setShortcut(_translate("MainWindow", "Backspace"))
-        self.actionForward.setText(_translate("MainWindow", "forward"))
+        self.actionForward.setText(_translate("MainWindow", Asc.forward))
         self.actionForward.setToolTip(_translate("MainWindow", "Go Forward"))
         self.actionForward.setShortcut(_translate("MainWindow", "Right"))
         self.actionReload.setText(_translate("MainWindow", "reload"))
-        self.actionReload.setToolTip(_translate("MainWindow", "Reload"))
+        self.actionReload.setToolTip(_translate("MainWindow", Asc.reload))
         self.actionReload.setShortcut(_translate("MainWindow", "Ctrl+R"))
         self.actionHome.setText(_translate("MainWindow", "home"))
-        self.actionHome.setToolTip(_translate("MainWindow", "Home"))
+        self.actionHome.setToolTip(_translate("MainWindow", Asc.home))
         self.actionHome.setShortcut(_translate("MainWindow", "Ctrl+H"))
 
 
 if __name__ == "__main__":
-    import sys
-
     app = QtWidgets.QApplication(sys.argv)
     app.setWindowIcon(QIcon(Asc.appIcon))
     app.setApplicationName(Asc.appName)
